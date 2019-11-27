348954332a1e8ff473a7da986f773d807181855c
richard visser <Richard_Visser@outlook.com>
2019-11-27 17:23:52 +0100

    added some navbar stuff error pages and metatitles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,4 +30,8 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)|(^profile$)/
   end
+
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
