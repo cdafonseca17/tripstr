a337b983eb33500748daa49e9bf2b3a3f2c60ec1
Caroline Dafonseca <dafonseca.ca@gmail.com>
2019-12-14 11:36:16 +0100

    mapbox completely removed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,13 @@
 import "bootstrap";
 // CSS
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+// import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 // internal imports
-import { initMapbox } from '../plugins/init_mapbox';
-
+// import { initMapbox } from '../plugins/init_mapbox';
+import activatePlacesSearch from '../plugins/google_autocomplete_api';
+// import initMap from '../plugins/google_map';
 // searching on mapbox map
 
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+// import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import smoothScroll from "../plugins/find_a_trip_auto_scroll";
 
@@ -16,9 +17,11 @@ import { initUpdateNavbarOnScroll } from '../plugins/init_navbar';
 
 initUpdateNavbarOnScroll();
 
-initMapbox();
+activatePlacesSearch();
 
 smoothScroll();
 
+initMap();
 
 
+// initMapbox();
